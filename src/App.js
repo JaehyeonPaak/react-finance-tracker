817171a8d30cbee4233722d0484cb76d8196c19a
wogus392 <wogus392@nexol.co.kr>
2023-05-04 18:14:41 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  // Imperative approach by giving step-by-step instructions
+  const para = document.createElement('p');
+  para.textContent = 'She take the world off my shoulders, if it was ever hard to move';
+  document.getElementById('root').append(para);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Glimpse of us - Joji</p>
+        <ExpenseItem></ExpenseItem>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
+// index.js is the first file to 
+
 import ReactDOM from 'react-dom/client';
+// ReactDOM is exporting some React DOM object
+// ReactDOM은 React로 작성된 UI를 실제 DOM에 렌더링하기 위한 라이브러리이다.
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Let ReactDOM know the main place where the React application gets rendered to 
+root.render(<App />);
+// Tell React what should be rendered in that div
+// ReactDOM.render() 메서드는 App 컴포넌트를 id가 root인 실제 DOM 요소에 렌더링한다.
