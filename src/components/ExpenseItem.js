07a3660db309b2ed972a0a0a1f0b979abd73ff3d
JaehyeonPaak <wogus392@gmail.com>
2023-06-04 01:44:58 +0900

    feat: passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css';
 // This simply tells the build process that the CSS file should be considered.
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmout = 294.58;
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
+                <h2>{props.title}</h2>
             </div>
-            <div className="expense-item__price">${expenseAmout}</div>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     );
 }
