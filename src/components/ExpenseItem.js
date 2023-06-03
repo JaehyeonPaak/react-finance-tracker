6a12c2a9e597988d8daf1864b62e01d20170d558
JaehyeonPaak <wogus392@gmail.com>
2023-06-03 18:49:18 +0900

    feat: outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,17 @@ import './ExpenseItem.css';
 // This simply tells the build process that the CSS file should be considered.
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmout = 294.58;
+
     return (
         <div className="expense-item">
-            <div>June 22th 1999</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car insurance</h2>
+                <h2>{expenseTitle}</h2>
             </div>
-            <div className="expense-item__price">$203.24</div>
+            <div className="expense-item__price">${expenseAmout}</div>
         </div>
     );
 }
