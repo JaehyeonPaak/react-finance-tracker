1f4b5b5ecaa8721e6b7f651f4e7b1f5502436d5a
JaehyeonPaak <wogus392@gmail.com>
2023-06-04 14:55:32 +0900

    feat: add composition (children props)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css';
 // This simply tells the build process that the CSS file should be considered.
 import ExpenseDate from './ExpenseDate.js';
+import Card from './Card.js';
 
 function ExpenseItem(props) {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
             </div>
             <div className="expense-item__price">${props.amount}</div>
-        </div>
+        </Card>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-// index.js is the first file to 
-
 import ReactDOM from 'react-dom/client';
-// ReactDOM is exporting some React DOM object
-// ReactDOM은 React로 작성된 UI를 실제 DOM에 렌더링하기 위한 라이브러리이다.
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// Let ReactDOM know the main place where the React application gets rendered to 
 root.render(<App />);
-// Tell React what should be rendered in that div
-// ReactDOM.render() 메서드는 App 컴포넌트를 id가 root인 실제 DOM 요소에 렌더링한다.
