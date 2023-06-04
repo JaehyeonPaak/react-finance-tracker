b157075c76a5d73516668afc22ff34573127e92f
JaehyeonPaak <wogus392@gmail.com>
2023-06-05 00:16:52 +0900

    feat: working with state hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
 import './ExpenseItem.css';
-// This simply tells the build process that the CSS file should be considered.
 import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card.js';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Update');
+        console.log(title);
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
-            <div className="expense-item__price">${props.amount}</div>
+            <button onClick={clickHandler}>click</button>
         </Card>
     );
 }
